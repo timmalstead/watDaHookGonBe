f8c8446a62d5faa0b9030331b2c9ae8d538244d1
Tim Malstead <timmmalstead@gmail.com>
2020-08-27 19:17:52 -0700

    completes image setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react"
+import albumcover from "./albumcover.jpg"
+
+const holder = { width: "100vw", height: "100vh" }
 
 export default () => {
-  return <div>hello</div>
+  return (
+    <div style={holder}>
+      <img src={albumcover} alt="Murphy's Law" />
+    </div>
+  )
 }
