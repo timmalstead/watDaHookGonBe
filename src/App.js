60f0036af3c5b73d4c2c06094c6564acd7464f38
Tim Malstead <timmmalstead@gmail.com>
2020-08-31 22:18:32 -0700

    finished one file demo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react"
+import React, { useState } from "react"
 import albumcover from "./albumcover.jpg"
 
 const holderStyle = { width: "100vw", height: "100vh" }
@@ -12,7 +12,6 @@ const { innerHeight, innerWidth } = window
 const startingPosition = { x: innerWidth / 2 - 100, y: innerHeight / 2 - 100 }
 
 export default () => {
-  const savedPosition = useRef(null)
   const [dragInfo, setDragInfo] = useState({
     isDragging: false,
     origin: { x: 0, y: 0 },
@@ -21,31 +20,57 @@ export default () => {
   })
 
   const { isDragging } = dragInfo
-
   const handleMouseDown = ({ clientX, clientY }) => {
-    if (!isDragging) {
-      let translation, lastTranslation
-
-      if (savedPosition.current) {
-        translation = savedPosition.current.translation
-        lastTranslation = savedPosition.current.lastTranslation
-      } else {
-        translation = startingPosition
-        lastTranslation = startingPosition
-      }
-
+    if (!isDragging)
       setDragInfo({
+        ...dragInfo,
         isDragging: true,
         origin: { x: clientX, y: clientY },
-        translation,
-        lastTranslation,
+      })
+  }
+
+  const handleMouseMove = ({ clientX, clientY }) => {
+    if (isDragging) {
+      const { origin, lastTranslation } = dragInfo
+      setDragInfo({
+        ...dragInfo,
+        translation: {
+          x: Math.abs(clientX - (origin.x + lastTranslation.x)),
+          y: Math.abs(clientY - (origin.y + lastTranslation.y)),
+        },
       })
     }
   }
 
+  const handleMouseUp = () => {
+    if (isDragging) {
+      const { translation } = dragInfo
+      setDragInfo({
+        ...dragInfo,
+        isDragging: false,
+        lastTranslation: { x: translation.x, y: translation.y },
+      })
+    }
+  }
+
+  const picturePosition = {
+    position: "absolute",
+    right: `${dragInfo.translation.x}px`,
+    bottom: `${dragInfo.translation.y}px`,
+  }
+
   return (
     <div style={holderStyle}>
-      <div style={pictureStyle} onMouseDown={handleMouseDown} />
+      <div
+        style={{
+          ...picturePosition,
+          ...pictureStyle,
+        }}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      />
     </div>
   )
 }
