386b0bc0b99276cdc1e7f73c403b086b69d3aea6
Tim Malstead <timmmalstead@gmail.com>
2020-08-31 22:42:43 -0700

    adds useDrag hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import React from "react"
+import useDrag from "./useDrag"
 import albumcover from "./albumcover.jpg"
 
 const holderStyle = { width: "100vw", height: "100vh" }
@@ -12,52 +13,12 @@ const { innerHeight, innerWidth } = window
 const startingPosition = { x: innerWidth / 2 - 100, y: innerHeight / 2 - 100 }
 
 export default () => {
-  const [dragInfo, setDragInfo] = useState({
-    isDragging: false,
-    origin: { x: 0, y: 0 },
-    translation: startingPosition,
-    lastTranslation: startingPosition,
-  })
-
-  const { isDragging } = dragInfo
-  const handleMouseDown = ({ clientX, clientY }) => {
-    if (!isDragging)
-      setDragInfo({
-        ...dragInfo,
-        isDragging: true,
-        origin: { x: clientX, y: clientY },
-      })
-  }
-
-  const handleMouseMove = ({ clientX, clientY }) => {
-    if (isDragging) {
-      const { origin, lastTranslation } = dragInfo
-      setDragInfo({
-        ...dragInfo,
-        translation: {
-          x: Math.abs(clientX - (origin.x + lastTranslation.x)),
-          y: Math.abs(clientY - (origin.y + lastTranslation.y)),
-        },
-      })
-    }
-  }
-
-  const handleMouseUp = () => {
-    if (isDragging) {
-      const { translation } = dragInfo
-      setDragInfo({
-        ...dragInfo,
-        isDragging: false,
-        lastTranslation: { x: translation.x, y: translation.y },
-      })
-    }
-  }
-
-  const picturePosition = {
-    position: "absolute",
-    right: `${dragInfo.translation.x}px`,
-    bottom: `${dragInfo.translation.y}px`,
-  }
+  const {
+    picturePosition,
+    handleMouseDown,
+    handleMouseMove,
+    handleMouseUp,
+  } = useDrag(startingPosition)
 
   return (
     <div style={holderStyle}>
